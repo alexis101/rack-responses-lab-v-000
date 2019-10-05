3dc08dc5febbbec6d44549c89dff029e7b661bbf
Alexis Kondilis <kondilisalexis@gmail.com>
2019-10-05 03:05:45 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+  if time.now.hour >= 12
+    resp.write "Good Afternoon!"
+  else  
+    resp.write "Good Morning!"
+
+    resp.finish
+  end
+end
